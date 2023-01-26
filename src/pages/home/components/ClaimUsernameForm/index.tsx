4bbebd5b4b52348125c9c2ebd14bf0ef2e270c38
Salinas <matheussalinas3@gmail.com>
2023-01-26 10:28:40 -0300

    Cadastro de usuário - Valor inicial do nome de usuário

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -4,13 +4,14 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { Form, FormAnnotation } from './styles'
+import { useRouter } from 'next/router'
 
 const ClaimUserNameFormSchema = z.object({
   username: z
     .string()
-    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras' })
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
     .regex(/^([a-z\\\\-]+)$/i, {
-      message: 'O usuário deve conter apenas letras e hifens',
+      message: 'O usuário deve conter apenas letras e hifens.',
     })
     .transform((username) => username.toLowerCase()),
 })
@@ -21,13 +22,17 @@ export function ClaimUsernameForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ClaimUserNameData>({
     resolver: zodResolver(ClaimUserNameFormSchema),
   })
 
+  const router = useRouter()
+
   async function handleClaimUsername(data: ClaimUserNameData) {
-    console.log(data)
+    const { username } = data
+
+    await router.push(`/register?username=${username}`)
   }
 
   return (
@@ -39,7 +44,7 @@ export function ClaimUsernameForm() {
           placeholder="seu-usuario"
           {...register('username')}
         />
-        <Button size="sm" type="submit">
+        <Button size="sm" type="submit" disabled={isSubmitting}>
           Reservar
           <ArrowRight />
         </Button>
